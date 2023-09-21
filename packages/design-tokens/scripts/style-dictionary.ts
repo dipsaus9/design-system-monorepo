549887258a17bc93ebb2ff1db1e000da6965164f
Dennis <dennis.spierenburg@hotmail.com>
2023-09-21 16:16:45 +0200

    style dictionary

--- packages/design-tokens/scripts/style-dictionary.ts
+++ packages/design-tokens/scripts/style-dictionary.ts
@@ -1,16 +1,24 @@
-import { rmSync } from "fs";
-
-import { registerTransforms } from "@tokens-studio/sd-transforms";
-
+import { transforms, registerTransforms } from "@tokens-studio/sd-transforms";
 import StyleDictionary from "style-dictionary";
 
-// will register them on StyleDictionary object
-// that is installed as a dependency of this package.
 registerTransforms(StyleDictionary);
 
-function init() {
-  // rmSync(`./build/`, { recursive: true, force: true });
+StyleDictionary.registerTransform({
+  type: "name",
+  name: "name/ts/cleanup",
+  transformer: (token) =>
+    token.name.replace(
+      /^spacing-|^border-radius-|^shadow-|^colors-|^border-width-|^border-tokens-|^typography-|^font-family-|^font-size-|^font-weight-|^line-height-|^letter-spacing-|^opacity-/,
+      ""
+    ),
+});
 
+StyleDictionary.registerTransformGroup({
+  name: "custom/tokens-studio",
+  transforms: [...transforms, "name/cti/kebab", "name/ts/cleanup"],
+});
+
+function init() {
   const sd = StyleDictionary.extend({
     source: ["./source/*.json"],
     platforms: {
@@ -19,8 +27,60 @@ function init() {
         buildPath: "dist/js/",
         files: [
           {
-            destination: "variables.js",
-            format: "javascript/es6",
+            destination: "all.js",
+            format: "javascript/module-flat",
+          },
+        ],
+      },
+      tailwind: {
+        transformGroup: "custom/tokens-studio",
+        buildPath: "dist/js/",
+        files: [
+          {
+            destination: "colors.js",
+            format: "javascript/module-flat",
+            filter: (token) => {
+              const type = token.type;
+
+              return (
+                type === "color" ||
+                type === "border" ||
+                type === "boxShadow" ||
+                type === "composition"
+              );
+            },
+          },
+          {
+            destination: "spacing.js",
+            format: "javascript/module-flat",
+            filter: (token) => {
+              return token.type === "spacing" || token.type === "composition";
+            },
+          },
+          {
+            destination: "shadow.js",
+            format: "javascript/module-flat",
+            filter: (token) => {
+              return token.type === "boxShadow" || token.type === "composition";
+            },
+          },
+          {
+            destination: "border-radius.js",
+            format: "javascript/module-flat",
+            filter: (token) => {
+              return (
+                token.type === "borderRadius" || token.type === "composition"
+              );
+            },
+          },
+          {
+            destination: "border-width.js",
+            format: "javascript/module-flat",
+            filter: (token) => {
+              return (
+                token.type === "borderWidth" || token.type === "composition"
+              );
+            },
           },
         ],
       },
