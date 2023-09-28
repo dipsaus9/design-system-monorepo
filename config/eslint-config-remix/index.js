f2c549d34bc00482653e5ca95a3bb442c021ce3a
Dennis <dennis.spierenburg@hotmail.com>
2023-09-28 13:51:54 +0200

    documentation

--- config/eslint-config-remix/index.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('eslint').Linter.Config} */
-module.exports = {
-  extends: [
-    "@dipsaus9/eslint-config",
-    "@remix-run/eslint-config",
-    "@remix-run/eslint-config/node",
-  ],
-};
---
+++ packages/ui/src/button/README.md
@@ -0,0 +1,13 @@
+# Button
+
+Button Atom
+
+## Usage
+
+Render a Button
+
+```tsx
+import { Button } from "@dipsaus9/ui/src/button";
+
+<Button intent="primary">Button text</Button>;
+```
